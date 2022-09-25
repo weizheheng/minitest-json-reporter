0fca9d3a2f8f8f25c8d1899678933962d36a3125
Wei Zhe Heng <tech@weizheheng.com>
2022-09-25 11:53:33 +0800

    The reporter is working now

--- lib/minitest/json_reporter_plugin.rb
+++ lib/minitest/json_reporter_plugin.rb
@@ -4,18 +4,44 @@ require "minitest"
 require "json"
 
 module Minitest
+  def self.plugin_json_reporter_init(options)
+    if options[:json]
+      Minitest.reporter.reporters.clear
+      Minitest.reporter << JsonReporter.new(options)
+    end
+  end
+
+  def self.plugin_json_reporter_options(opts, options)
+    opts.on("--json", "Json dump") { |_json| options[:json] = true }
+  end
+
   class JsonReporter < AbstractReporter
-    def start
+    attr_accessor :io, :results
+
+    def initialize(options)
+      @io = options[:io]
+      @results = []
     end
 
-    def record
+    def record(result)
+      line = result.source_location[-1]
+      failure = result.failures[0]
+
+      if failure
+        # "/Users/marcusheng/projects/rails/budgetyourtime/test/models/user_test.rb:11"
+        line = failure.location.split("/")[-1].split(":")[-1]
+      end
+
+      results << {
+        name: result.name,
+        status: result.passed? ? "PASS" : "FAIL",
+        failures: failure.to_s,
+        line: line.to_i
+      }.to_json
     end
 
     def report
-      super
-
-      json_report = { test: "name of the test" }
-      io.write(JSON.dump(json_report))
+      results.each { |result| io.puts result }
     end
   end
 end
